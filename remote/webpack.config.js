41c64986cd6d418cae7eeb2977c266fe19faf2c9
oldtimestj <85237486@qq.com>
2021-02-05 15:41:33 +0800

    webpack-master

--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -33,14 +33,6 @@ module.exports = {
     new ModuleFederationPlugin({
       name: 'remoteVar',  // 向外暴露的全局变量名
       filename: 'remoteEntry.js', //构建出的文件名
-
-      // remotes: {
-      //   host: 'hostVar@http://localhost:9393/remoteEntry.js'
-      // },
-      remotes: {
-        host: 'hostVar@http://localhost:9393/remoteEntry.js',
-        app1: "app1@http://localhost:3000/remoteEntry.js",
-      },
       exposes: {  // 暴露出给外部使用的组件
         './NewsList': './src/NewsList11111'
       },
